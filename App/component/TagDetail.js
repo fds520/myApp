debd432e2cd78e5485c1ff4cfe8bc257ebdf9e94
fds <2296005140@qq.com>
2018-03-05 16:12:54 +0800

    页面布局

--- App/component/TagDetail.js
+++ App/component/TagDetail.js
@@ -3,17 +3,17 @@
 import React from 'react';
 
 import {
-  StyleSheet,
-  Text,
-  Image,
-  View,
-  Dimensions,
-  TouchableOpacity,
+    StyleSheet,
+    Text,
+    Image,
+    View,
+    Dimensions,
+    TouchableOpacity,
 } from 'react-native';
 
 import URLConf from '../api/URLConf';
-// import TagFollow from './actions/TagFollow';
-// import TagFeedList from './TagFeeds';
+import TagFollow from './actions/TagFollow';
+import TagFeedList from './TagFeeds';
 import ParallaxScrollView from 'react-native-parallax-scroll-view';
 
 const IMAGE_BASE_URL = URLConf.IMG_BASE_URL;
@@ -26,144 +26,131 @@ const tag_thumbnail = '?imageView2/1/w/'+windowWidth+'/h/'+PARALLAX_HEADER_HEIGH
 var TagDetail = React.createClass({
 
 
-  back: function(){
-    this.props.navigator.pop();
-  },
+    back: function(){
+        this.props.navigator.pop();
+    },
 
-  nav2TagDetail: function(tag) {
-    console.log(tag.tag);
-    this.props.navigator.push({
-      title: tag.tag,
-      component: TagDetail,
-      params: {tag: tag}
-    });
-  },
+    nav2TagDetail: function(tag) {
+        console.log(tag.tag);
+        this.props.navigator.push({
+            title: tag.tag,
+            component: TagDetail,
+            params: {tag: tag}
+        });
+    },
 
 
-  render: function(){
-    return (
-      <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'white'}}>
-      <ParallaxScrollView
-        backgroundColor="rgba(255,255,255,1)"
-        headerBackgroundColor="#333"
-        stickyHeaderHeight={ STICKY_HEADER_HEIGHT }
-        parallaxHeaderHeight={ PARALLAX_HEADER_HEIGHT }
-        backgroundSpeed={10}
-        style={{flex:1}}
+    render: function(){
+        return (
+            <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'white'}}>
+                <ParallaxScrollView
+                    backgroundColor="rgba(255,255,255,1)"
+                    headerBackgroundColor="#333"
+                    stickyHeaderHeight={ STICKY_HEADER_HEIGHT }
+                    parallaxHeaderHeight={ PARALLAX_HEADER_HEIGHT }
+                    backgroundSpeed={10}
+                    style={{flex:1}}
 
-        renderBackground={() => (
-          <View key="background">
-            <Image resizeMode='cover' style={styles.headerImg} source={{uri: IMAGE_BASE_URL + this.props.tag.cover + tag_thumbnail}} />
-            <View style={{position: 'absolute',
-                          top: 0,
-                          width: windowWidth,
-                          backgroundColor: 'rgba(0,0,0,.4)',
-                          height: PARALLAX_HEADER_HEIGHT}}/>
-          </View>
-        )}
+                    renderBackground={() => (
+                        <View key="background">
+                            <Image resizeMode='cover' style={styles.headerImg} source={{uri: IMAGE_BASE_URL + this.props.tag.cover + tag_thumbnail}} />
+                            <View style={{position: 'absolute',
+                                top: 0,
+                                width: windowWidth,
+                                backgroundColor: 'rgba(0,0,0,.4)',
+                                height: PARALLAX_HEADER_HEIGHT}}/>
+                        </View>
+                    )}
 
-        renderForeground={() => (
-          <View key="parallax-header" style={ styles.parallaxHeader }>
-            <View style={{flex: 1, flexDirection: 'row',justifyContent: 'center',alignItems: 'center', position: 'absolute',left:20, bottom: 40, backgroundColor: 'rgba(0,0,0,0)',}}>
-              <Text style={{color: 'white', fontSize: 24, opacity: 0.9, marginRight: 10}}>#{this.props.tag.tag}</Text>
+                    renderForeground={() => (
+                        <View key="parallax-header" style={ styles.parallaxHeader }>
+                            <View style={{flex: 1, flexDirection: 'row',justifyContent: 'center',alignItems: 'center', position: 'absolute',left:20, bottom: 40, backgroundColor: 'rgba(0,0,0,0)',}}>
+                                <Text style={{color: 'white', fontSize: 24, opacity: 0.9, marginRight: 10}}>#{this.props.tag.tag}</Text>
+                            </View>
+                        </View>
+                    )}
+
+                    renderStickyHeader={() => (
+                        <View key="sticky-header" style={styles.stickySection}>
+                            <Text style={styles.stickySectionText}>{this.props.tag.tag}</Text>
+                        </View>
+                    )}
+
+                    renderFixedHeader={() => (
+                        <View key="fixed-header" style={styles.fixedSection}>
+                            <View style={{left: -(windowWidth-112), bottom: -5,}}>
+                                <TouchableOpacity onPress={()=>this.props.navigator.pop()}>
+                                    <Image style={{width: 18, height: 18}} source={require('../imgs/back.png')} />
+                                </TouchableOpacity>
+                            </View>
+                            <TagFollow token={this.props.token} refresh={this.props.refresh} tagId={this.props.tag.id}/>
+                        </View>
+                    )}>
+
+                    <TagFeedList tagId={this.props.tag.id} token={this.props.token} navigator={this.props.navigator} nav2TagDetail={this.nav2TagDetail} refresh={this.props.refresh}/>
+                </ParallaxScrollView>
             </View>
-          </View>
-        )}
-
-        renderStickyHeader={() => (
-          <View key="sticky-header" style={styles.stickySection}>
-            <Text style={styles.stickySectionText}>{this.props.tag.tag}</Text>
-          </View>
-        )}
-
-        renderFixedHeader={() => (
-          <View key="fixed-header" style={styles.fixedSection}>
-            <View style={{left: -(windowWidth-112), bottom: -5,}}>
-              <TouchableOpacity onPress={()=>this.props.navigator.pop()}>
-                <Image style={{width: 18, height: 18}} source={require('../imgs/back.png')} />
-              </TouchableOpacity>
-            </View>
-            /*<TagFollow token={this.props.token} refresh={this.props.refresh} tagId={this.props.tag.id}/>*/
-          </View>
-          )}>
-
-          /*<TagFeedList tagId={this.props.tag.id} token={this.props.token} navigator={this.props.navigator} nav2TagDetail={this.nav2TagDetail} refresh={this.props.refresh}/>*/
-
-          {/* <ScrollableTabView
-                style={{marginTop: 10, }}
-                tabBarUnderlineStyle={{backgroundColor: '#00B5AD', height: 2,}}
-                tabBarActiveTextColor={'rgb(0,0,0)'}
-                initialPage={0}
-                renderTabBar={() => <ScrollableTabBar />}
-              >
-                <ScrollView tabLabel='热门' style={{borderBottomColor: '#00B5AD'}}><TagFeedList token={this.props.token} navigator={this.props.navigator} nav2TagDetail={this.nav2TagDetail}/></ScrollView>
-                <ScrollView tabLabel='最新'><TagFeedList token={this.props.token} navigator={this.props.navigator} nav2TagDetail={this.nav2TagDetail}/></ScrollView>
-              </ScrollableTabView> */}
-
-        </ParallaxScrollView>
-
-      </View>
-    );
-  },
+        );
+    },
 });
 
 var styles = StyleSheet.create({
-  navContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    height:50,
-    backgroundColor: '#00B5AD',
-  },
-  back: {
-    marginLeft:10,
-    marginTop:23,
-  },
-  title: {
-    alignSelf:'center',
-    marginTop: 10,
-  },
-  right: {
-    marginRight:10,
-    marginTop:23,
-  },
-  header: {
-    height: 180,
-    position: 'relative',
-  },
-  feedList: {
+    navContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height:50,
+        backgroundColor: '#00B5AD',
+    },
+    back: {
+        marginLeft:10,
+        marginTop:23,
+    },
+    title: {
+        alignSelf:'center',
+        marginTop: 10,
+    },
+    right: {
+        marginRight:10,
+        marginTop:23,
+    },
+    header: {
+        height: 180,
+        position: 'relative',
+    },
+    feedList: {
 
-  },
-  parallaxHeader: {
-    alignItems: 'flex-end',
-    flex: 1,
-    flexDirection: 'row',
-    paddingLeft: 20,
-    paddingBottom: 20,
-  },
+    },
+    parallaxHeader: {
+        alignItems: 'flex-end',
+        flex: 1,
+        flexDirection: 'row',
+        paddingLeft: 20,
+        paddingBottom: 20,
+    },
 
-  headerImg: {
-    height: PARALLAX_HEADER_HEIGHT,
-  },
+    headerImg: {
+        height: PARALLAX_HEADER_HEIGHT,
+    },
 
-  stickySection: {
-    height: STICKY_HEADER_HEIGHT,
-    width: windowWidth,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 13,
-  },
-  stickySectionText: {
-    color: 'black',
-    fontSize: 18,
-  },
-  fixedSection: {
-    flex: 1,
-    flexDirection: 'row',
-    position: 'absolute',
-    bottom: 10,
-    right: 20
-  },
+    stickySection: {
+        height: STICKY_HEADER_HEIGHT,
+        width: windowWidth,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 13,
+    },
+    stickySectionText: {
+        color: 'black',
+        fontSize: 18,
+    },
+    fixedSection: {
+        flex: 1,
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 10,
+        right: 20
+    },
 
 });
 
